e95de312e0fd4cb9462faa47731f1e4627b56a50
Przemysław Drzewicki <przemyslaw.drzewicki@gmail.com>
2022-01-17 21:43:16 +0100

    (feat) DividerAtom - add new component

---
+++ panel/src/atomic/atoms/DividerAtom/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="['DividerAtom', {
+    'DividerAtom--color': color,
+  }]">
+    <slot v-if="Object.keys($slots).length > 0 && dividerType === 'horizontal'"/>
+  </div>
+</template>
+<script src="./component.ts" lang="ts" />
+<style src="./style.sass" lang="sass"></style>
