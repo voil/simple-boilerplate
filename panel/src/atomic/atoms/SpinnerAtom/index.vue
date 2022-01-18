44362cefee26ee2d4edacb946e06242739523e8c
Przemysław Drzewicki <przemyslaw.drzewicki@gmail.com>
2022-01-18 12:27:39 +0100

    (feat) SpinnerAtom - add new component

---
+++ panel/src/atomic/atoms/SpinnerAtom/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="SpinnerAtom">
+    <span v-if="message"
+      class="SpinnerAtom__message"
+      data-testid="spinnerMessageAtomInstance"
+    >
+      {{ message }}
+    </span>
+    <div class="SpinnerAtom__spinner" />
+    <div class="SpinnerAtom__content">
+      <slot />
+    </div>
+  </div>
+</template>
+<script src="./component.ts" lang="ts" />
+<style src="./style.sass" lang="sass"></style>
---
+++ panel/src/atomic/atoms/SpinnerAtom/stories.ts
@@ -0,0 +1,44 @@
+import SpinnerAtomComponent from './index.vue';
+
+/**
+ * @type ComponentPropsType
+ */
+type ComponentPropsType = {
+  message: string;
+};
+
+/**
+ * Export default component.
+ */
+export default {
+  component: SpinnerAtomComponent,
+  title: 'UI/Atoms/Spinner Atom',
+  argTypes: {
+    message: {
+      name: 'message',
+      description: 'Prop for message spinner.',
+    },
+  },
+  parameters: {
+    jest: ['spinnerAtom.spec.ts'],
+    docs: {
+      description: {
+        component: 'Spinner component.',
+      },
+      source: {
+        code: '<SpinnerAtom message="message" />',
+      },
+    },
+  },
+};
+
+/**
+ * Export storie for component.
+ */
+export const SpinnerAtom = (args: ComponentPropsType) => ({
+    components: { SpinnerAtomComponent },
+    setup() {
+      return { args };
+    },
+    template: '<SpinnerAtomComponent v-bind="args"><div>Lorem ipsum</div></SpinnerAtomComponent>',
+});
