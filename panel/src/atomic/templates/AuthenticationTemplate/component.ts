bdf6cccaa34a73314fa9a6519e714304572cb4f1
Przemysław Drzewicki <przemyslaw.drzewicki@gmail.com>
2022-01-19 14:23:57 +0100

    (feat) AuthenticationTemplate - create new component

---
+++ panel/src/atomic/templates/AuthenticationTemplate/component.ts
@@ -0,0 +1,37 @@
+import {
+  defineComponent,
+  defineAsyncComponent,
+} from 'vue';
+
+/**
+ * Authentication
+ * Component template authentication.
+ *
+ * @author Przemysław Drzewicki <przemyslaw.drzewicki@gmail.com>
+ */
+export default defineComponent({
+  name: 'AuthenticationTemplate',
+
+  components: {
+    CardAtom: defineAsyncComponent(() => import('@/atomic/atoms/CardAtom/index.vue')),
+    LogoAtom: defineAsyncComponent(() => import('@/atomic/atoms/LogoAtom/index.vue')),
+  },
+
+  props: {
+    /**
+     * Prop for title.
+     */
+    title: {
+      type: String,
+      default: null,
+    },
+
+    /**
+     * Prop for description.
+     */
+    description: {
+      type: String,
+      default: null,
+    },
+  },
+});
---
+++ panel/src/atomic/templates/AuthenticationTemplate/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="AuthenticationTemplate">
+    <div class="AuthenticationTemplate__content">
+      <LogoAtom image="/assets/images/logo.png"
+        class="AuthenticationTemplate__logo"/>
+      <CardAtom
+        :cardTitle="title"
+        :cardDescription="description"
+      >
+        <slot />
+      </CardAtom>
+    </div>
+  </div>
+</template>
+<script src="./component.ts" lang="ts" />
+<style src="./style.sass" lang="sass"></style>
